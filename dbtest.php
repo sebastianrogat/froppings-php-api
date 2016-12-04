6514f8958052d58d5aa784c9726e46daed50a35d
Erika Gutiérrez <erikagutierrez01@gmail.com>
2016-12-03 20:39:28 -0400

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost = getenv("localhost");
-$dbport = getenv("3306");
-$dbuser = getenv("erikagtierrez");
-$dbpwd = getenv("eri33vgc66");
-$dbname = getenv("froppingsdatabase");
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
